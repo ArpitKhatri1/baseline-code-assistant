c6e790f9feb080491c567f1585849792091f7084
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-09-17 19:48:08 +0530

    test commit

--- baseline-compat-assistant/src/extension.ts
+++ baseline-compat-assistant/src/extension.ts
@@ -6,7 +6,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const disposable = vscode.commands.registerCommand('baseline-compat-assistant.helloWorld', () => {
 		
-		vscode.window.showInformationMessage('Hello World from baseline-compat-assistant!');
+		vscode.window.showInformationMessage('Hello World from baseline-comspat-assistant!');
 	});
 
 	context.subscriptions.push(disposable);
