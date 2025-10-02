f8321fcf0c1fcd72be8e9d0b8bbf1f28c6c16f0e
ArpitKhatri1 <arpitkumarkhatri@gmail.com>
2025-10-02 23:03:27 +0530

    added eslint support

--- baseline-compat-assistant/src/quick-test/test.ts
+++ baseline-compat-assistant/src/quick-test/test.ts
@@ -1,6 +1,11 @@
-(async () => {
-  const { getStatus, computeBaseline } = await import("compute-baseline");
-  const status = computeBaseline({ compatKeys: ["css.pseudo-elements.marker"] });
+// (async () => {
+//   const { getStatus, computeBaseline } = await import("compute-baseline");
+//   const status = computeBaseline({ compatKeys: ["css.pseudo-elements.marker"] });
 
-  console.log(status.toJSON());
-})();
+//   console.log(status.toJSON());
+// })();
+
+import csstree from "css-tree";
+const css = "a { color: red; }";
+const ast = csstree.parse(css);
+console.log(ast);
